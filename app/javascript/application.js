1f09e9226e8cd91d5a532d53b671b4139011c3f7
Kenneth Ronquillo <keronquillo@gbox.adnu.edu.ph>
2025-11-20 16:09:20 +0800

    add overview section and edit events section. Closes #25.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,3 +14,31 @@ document.addEventListener("turbo:load", () => {
   });
 });
 
+document.addEventListener("turbo:load", () => {
+  const modal = document.getElementById("confirmDeleteModal");
+  const confirmBtn = document.getElementById("confirmDeleteBtn");
+
+  if (!modal) return;
+
+  modal.addEventListener("show.bs.modal", event => {
+    const button = event.relatedTarget;
+    const url = button.getAttribute("data-student-url");
+    confirmBtn.setAttribute("href", url);
+    confirmBtn.setAttribute("data-turbo-method", "delete");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.fade-left, .fade-right');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.2 });
+
+  elements.forEach(el => observer.observe(el));
+});
